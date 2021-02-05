3959238b5c60389c6ccc75e67ff502f29375079b
anilakodukula <anilakodukula@gmail.com>
2021-02-05 12:50:48 -0500

    connecting frontend to backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 
+
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from "pusher-js";
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() =>{
+    const pusher = new Pusher('0c1f78eda580dce2fe29', {
+      cluster: 'us2'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });//once
+  }, [])
   return (
     <div className="app">
       <div className="app_body">
